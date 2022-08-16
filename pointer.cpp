3d47bf91e43c92affb7183a32790ac1033617d8b
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-08-16 03:56:33 -0700

    Add files via upload

--- pointer.cpp
+++ pointer.cpp
@@ -5,12 +5,25 @@ using  namespace std;
 
 
 int main() {
-    int i = 10;
+    /* int i = 10;
     cout << &i << endl;
     int *p = &i;
+    int * q = p;
+
 
     cout << p << endl;
     cout << *p << endl;
+    cout << i << endl;
+    cout << *q << endl;
+
+
+
+    for(int j = 0 ;j< 10 ;j++) {
+        i++;
+    }
+    
+     cout << i << endl;
+     cout << *p << endl;
 
 
     float u = 10.12121;
@@ -18,6 +31,18 @@ int main() {
     cout << &u << endl;
     cout << address << endl;
     cout << *address << endl;
+*/
+   int i = 10;
+   int * p = &i;
+   cout << p << endl;
+
+   p  = p + 1;
+   cout << p << endl;
+    p--;
+    cout << p << endl;
+    
+
+
 
 
 
