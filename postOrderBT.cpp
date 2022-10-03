9cf8a5cc5e94090724c9e8680b78568d94edf4a8
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-10-02 22:40:02 -0700

    Create postOrderBT.cpp

---
+++ postOrderBT.cpp
@@ -0,0 +1,14 @@
+
+
+void postOrder(BinaryTree<int>*root) 
+
+{
+
+     if(root == NULL) return;
+     
+     postOrder(root->left);
+     postOrder(root->right);
+     cout << root->data << " ";
+     
+     
+     }
