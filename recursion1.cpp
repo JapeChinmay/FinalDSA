6b2fe6a09c2500df75be3d3b647e11e186ae699a
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-10-04 12:23:52 -0700

    Add files via upload

---
+++ recursion1.cpp
@@ -0,0 +1,123 @@
+// #include<iostream>
+// using namespace std;
+
+
+// void print(int n) {
+
+//     if(n==0) return;
+//     cout << "chinmay" << endl;
+//     print(n-1);
+
+// }
+
+// // sum of n nums
+// int sqaure(int n) {
+//     if(n==0) return 0;
+
+//     int s =  n*n;
+//       return sqaure(n-1) + s;
+
+
+
+   
+
+    
+
+
+    
+// }
+
+// void name(int n) {
+//     if(n==0) return;
+//     if(n%2) {
+//         cout << "jape" << " ";
+//     }
+
+//     cout << "chinmay " << " ";
+
+//     cout << endl;
+
+//     return  name(n-1);
+
+
+// }
+
+// void printN(int n) {
+
+//     if(n==0) return;
+
+//      printN(n-1);
+//     if(n%2) cout <<  "** :" << n << endl;
+//     else {
+//     cout << n << endl;
+//     }
+
+//     //   print(n);
+
+//     return;
+
+
+
+
+
+// }
+
+// int p(int n) {
+//     if(n==0) return 1;
+//     if(n == 1) return 1;
+
+//     int s = n * p(n-1);
+//     return s;
+
+// }
+
+// // void nameNNumber(int n) {
+
+// //     if(n == 0) return;
+
+// //     cout << "chinmay" << " " << n*n;
+// //      return nameNNumber(n-1);
+
+// // }
+
+
+  
+
+
+
+
+
+
+
+
+
+// int main() {
+
+//     int n;
+//     cout << "enter n";
+//     cin >> n;
+//     cout << p(n);
+    
+
+
+// //    nameNNumber(n);
+
+    
+
+
+
+
+//     // int n;
+
+//     // cout  << "Enter n" << endl;
+
+//     // cin >> n;
+//     // print(n);
+
+
+
+
+
+//     /
+
+// }
