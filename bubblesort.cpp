ee14533ef2ac31f5532c0c85bf53a3f1df2c29c2
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-10-08 12:32:23 -0700

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,11 +3,17 @@ void bubbleSort(vector<int>& arr, int n)
 {   
     // Write your code here.
     for(int i=1;i<n;i++) {
+        
+          bool t = false;
+        
    
           for(int j=0;j<n-i;j++) {
                if(arr[j] > arr[j+1]){
                     swap(arr[j],arr[j+1]);
+                     t = true;
                }
           }
+        
+        if(t == false) break;
     }
 }
