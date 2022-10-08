b0178436d31a85d8e6ecb09616cad896fd1fc239
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-10-08 05:01:33 -0700

    Create countPrimes.cpp

---
+++ countPrimes.cpp
@@ -0,0 +1,38 @@
+
+
+class Solution {
+    
+
+  
+public:
+    int countPrimes(int n) {
+
+        int ans = 0;
+        vector<bool>primes(n+1, true);
+         
+
+         primes[0] = primes[1]  = false;
+
+
+         for(int i=2;i<n;i++) {
+                if(primes[i]) {
+                      ans++;
+
+                      for(int j = 2*i;j<n;j=j+i) {
+                            primes[j] = 0;
+
+
+                      }
+                }
+         }
+
+
+         return ans;
+
+
+
+         
+
+        
+    }
+};
